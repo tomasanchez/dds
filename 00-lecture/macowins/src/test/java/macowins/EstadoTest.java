35c0f813121a61b446cb61b9ff3145d1877c7928
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-13 11:28:56 -0300

    test: update test to match new status code

--- 00-lecture/macowins/src/test/java/macowins/EstadoTest.java
+++ 00-lecture/macowins/src/test/java/macowins/EstadoTest.java
@@ -14,19 +14,19 @@ public class EstadoTest {
 
     @Test
     public void estadoNuevo() {
-        Estado nuevo = new Estado("Nuevo", "No modifica el Precio Base", (p, d) -> p);
+        Estado nuevo = new Estado(Status.NUEVO, "Nuevo", "No modifica el Precio Base", (p, d) -> p);
         Assertions.assertEquals(25.5, nuevo.obtenerPrecio(25.5, 10));
     }
 
     @Test
     public void estadoLiquidacion() {
-        Estado liquidacion = new Estado("Liquidacion", "50% off", (p, d) -> p * 0.5);
+        Estado liquidacion = new Estado(Status.LIQ, "Liquidacion", "50% off", (p, d) -> p * 0.5);
         Assertions.assertEquals(10, liquidacion.obtenerPrecio(20, 0));
     }
 
     @Test
     public void estadoPromocion() {
-        Estado promocion = new Estado("Promoción", "Descuento ingresado", (p, d) -> (p - d));
+        Estado promocion = new Estado(Status.PROMO, "Promoción", "Descuento ingresado", (p, d) -> (p - d));
         Assertions.assertEquals(150.75, promocion.obtenerPrecio(200.75, 50));
     }
 }
