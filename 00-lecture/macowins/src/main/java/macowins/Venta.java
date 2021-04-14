7ac7e7d3ab4dfd2b8a3b14bfc7bba0ad63cb9a33
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-14 01:21:35 -0300

    feat: add collection of Prenda

--- 00-lecture/macowins/src/main/java/macowins/Venta.java
+++ 00-lecture/macowins/src/main/java/macowins/Venta.java
@@ -1,6 +1,8 @@
 package macowins;
 
 import java.time.LocalDate; // import the LocalDate class
+import java.util.ArrayList; // Fpr Collection
+import java.util.Optional; // For Array Iteration
 import java.util.Random; // Import the Random generator
 
 /**
@@ -14,7 +16,7 @@ public class Venta {
     /**
      * Número de venta
      */
-    long nroDeVenta;
+    final long nroDeVenta;
     /**
      * Cantidad de items de la venta
      */
@@ -22,11 +24,17 @@ public class Venta {
     /**
      * Número de Cliente, o DNI.
      */
-    String nroDeDeudor;
+    final String nroDeDeudor;
     /**
      * Fecha de venta.
      */
     LocalDate fecha;
+
+    /**
+     * Listado de Prendas
+     */
+    ArrayList<Prenda> prendas;
+
     /**
      * Total del Precio Base de cada item.
      */
@@ -44,12 +52,86 @@ public class Venta {
      */
     double total;
 
+    /**
+     * @param deudor - el cliente que realizón la compra
+     */
     public Venta(String deudor) {
 
         Random rand = new Random();
-
-        nroDeVenta = rand.nextLong();
+        prendas = new ArrayList<Prenda>();
+        nroDeVenta = Math.abs(rand.nextLong());
         nroDeDeudor = deudor;
         fecha = LocalDate.now();
     }
+
+    /**
+     * Genera una venta segun un carrito.
+     * 
+     * @param deudor  - el cliente que realizón la compra
+     * @param carrito - las prendas vendidas
+     */
+    public Venta(String deudor, ArrayList<Prenda> carrito) {
+
+        Random rand = new Random();
+        nroDeVenta = Math.abs(rand.nextLong());
+        nroDeDeudor = deudor;
+        fecha = LocalDate.now();
+        prendas = new ArrayList<Prenda>();
+
+        if (!carrito.isEmpty()) {
+            carrito.forEach(prenda -> agregarPrenda(prenda));
+            subtotal = carrito.stream().mapToDouble(p -> p.precio).sum();
+        }
+    }
+
+    /**
+     * Calcula contables y vende las prendas.
+     */
+    public void vender() {
+
+        prendas.forEach(prenda -> prenda.vender(nroDeVenta));
+        subtotal = prendas.stream().mapToDouble(prenda -> prenda.precio).sum();
+        total = prendas.stream().mapToDouble(prenda -> prenda.total).sum();
+        recargos = 0;
+        descuentos = prendas.stream().mapToDouble(prenda -> prenda.descuentos).sum();
+
+    }
+
+    /**
+     * Calcula contables y vende las prendas en las cuotas indicadas
+     * 
+     * @param cuotas - las cuotas en las que se financia la venta
+     */
+    public void vender(int cuotas) {
+
+        prendas.forEach(prenda -> prenda.vender(nroDeVenta, cuotas));
+        subtotal = prendas.stream().mapToDouble(prenda -> prenda.precio).sum();
+        total = prendas.stream().mapToDouble(prenda -> prenda.total).sum();
+        recargos = prendas.stream().mapToDouble(prenda -> prenda.recargos).sum();
+        descuentos = prendas.stream().mapToDouble(prenda -> prenda.descuentos).sum();
+    }
+
+    /**
+     * Agrega una prenda al carrito.
+     * 
+     * @param prenda la prenda a anadirse
+     */
+    public void agregarPrenda(Prenda prenda) {
+
+        Optional<Prenda> existente = prendas.stream()
+                .filter(p -> p.nroDePrenda == prenda.nroDePrenda && p.estado() == prenda.estado()).findFirst();
+
+        if (existente.isPresent()) {
+            existente.get().agregarItem();
+        } else {
+            prendas.add(prenda);
+        }
+
+        items++;
+    }
+
+    public double ganancia() {
+        return prendas.stream().mapToDouble(prenda -> prenda.ganancia()).sum();
+    }
+
 }
