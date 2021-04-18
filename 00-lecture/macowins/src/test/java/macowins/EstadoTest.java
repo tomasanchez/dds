27aff4ba1e9404d440e3a8fb718c8874ce9a0d2d
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-18 01:18:03 -0300

    test: Estado 2.0 passed

--- 00-lecture/macowins/src/test/java/macowins/EstadoTest.java
+++ 00-lecture/macowins/src/test/java/macowins/EstadoTest.java
@@ -7,26 +7,47 @@ import org.junit.jupiter.api.Assertions;
  * Pruebas de Estados
  *
  * @author Tomás Sánchez
- * @version 1.0
- * @since 04.13.2021
+ * @version 2.0
+ * @since 04.18.2021
  */
 public class EstadoTest {
 
-    @Test
-    public void estadoNuevo() {
-        Estado nuevo = new Estado(Status.NUEVO, "Nuevo", "No modifica el Precio Base", (p, d) -> p);
-        Assertions.assertEquals(25.5, nuevo.obtenerPrecio(25.5, 10));
-    }
+    private static final double precioSuperElevado = 1999.99;
+    private static final double precioElevado = 999.99;
+    private static final double precioModerado = 499.99;
+    private static final double precioBarato = 199.99;
+
+    private static final double descuentoNada = 0.0;
+    private static final double descuentoImportante = 200.0;
+    private static final double descuentoModerado = 50.0;
+    private static final double descuentoChico = 10;
 
     @Test
-    public void estadoLiquidacion() {
-        Estado liquidacion = new Estado(Status.LIQ, "Liquidacion", "50% off", (p, d) -> p * 0.5);
-        Assertions.assertEquals(10, liquidacion.obtenerPrecio(20, 0));
+    public void estadoNuevo() {
+
+        Assertions.assertEquals(precioElevado, Estado.nuevo.precio(precioElevado, descuentoModerado));
+        Assertions.assertEquals(precioModerado, Estado.nuevo.precio(precioModerado, descuentoChico));
+        Assertions.assertEquals(precioBarato, Estado.nuevo.precio(precioBarato, descuentoNada));
+        Assertions.assertEquals(precioSuperElevado, Estado.nuevo.precio(precioSuperElevado, descuentoImportante));
     }
 
     @Test
     public void estadoPromocion() {
-        Estado promocion = new Estado(Status.PROMO, "Promoción", "Descuento ingresado", (p, d) -> (p - d));
-        Assertions.assertEquals(150.75, promocion.obtenerPrecio(200.75, 50));
+        Assertions.assertEquals(precioElevado - descuentoNada, Estado.promocion.precio(precioElevado, descuentoNada));
+        Assertions.assertEquals(precioModerado - descuentoChico,
+                Estado.promocion.precio(precioModerado, descuentoChico));
+        Assertions.assertEquals(precioBarato - descuentoModerado,
+                Estado.promocion.precio(precioBarato, descuentoModerado));
+        Assertions.assertEquals(precioSuperElevado - descuentoImportante,
+                Estado.promocion.precio(precioSuperElevado, descuentoImportante));
     }
+
+    @Test
+    public void estadoLiquidacion() {
+        Assertions.assertEquals(precioElevado * 0.5, Estado.liquidacion.precio(precioElevado, descuentoModerado));
+        Assertions.assertEquals(precioBarato * 0.5, Estado.liquidacion.precio(precioBarato, descuentoImportante));
+        Assertions.assertEquals(precioModerado * 0.5, Estado.liquidacion.precio(precioModerado, descuentoChico));
+        Assertions.assertEquals(precioSuperElevado * 0.5, Estado.liquidacion.precio(precioSuperElevado, descuentoNada));
+    }
+
 }
