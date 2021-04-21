441ac7999f9d07016c4d55a7c5eda5af2ad07415
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-21 19:21:39 -0300

    fix(qmp): typo in Color class

--- 01-lecutre/quemepongo/src/main/java/quemepongo/Color.java
+++ 01-lecutre/quemepongo/src/main/java/quemepongo/Color.java
@@ -4,9 +4,9 @@ package quemepongo;
  * Dominio de Colores
  *
  * @author Tomás Sánchez
- * @version 1.0
+ * @version 1.1
  * @since 04.21.2021
  */
 public enum Color {
-    NIGUNO, ROJO, AZUL, AMARILLO
+    NINGUNO, ROJO, AZUL, AMARILLO
 }
