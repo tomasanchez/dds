ef7755487be97e2c9aa8830e996a5c1da88b913e
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-25 18:56:11 -0300

    refactor(qmp): material class

--- 01-lecture/quemepongo/src/main/java/quemepongo/Material.java
+++ 01-lecture/quemepongo/src/main/java/quemepongo/Material.java
@@ -1,12 +1,64 @@
 package quemepongo;
 
+import java.util.Objects;
+
 /**
- * Dominio de Materiales
+ * Material de una Prenda.
  *
- * @author Tomás Sánchez
- * @version 1.0
- * @since 04.21.2021
+ * @version 2.0
+ * @since 04.25.2021
  */
-public enum Material {
-    ALGODON, JEAN, LANA, LINO
+public class Material {
+
+    /**
+     * El material en sí.
+     *
+     * @since 2.0
+     */
+    TipoMaterial nombre;
+
+    /**
+     * La trama del material.
+     *
+     * @since 2.0
+     */
+    TramaTela trama;
+
+    /**
+     * Instancia un material
+     *
+     * @param nombre El material en sí
+     * @param trama
+     * @throws MaterialInvalido si no se cumplen las condiciones.
+     * @since 2.0
+     */
+    public Material(TipoMaterial nombre, TramaTela trama) {
+
+        if (!this.nombre.admiteTrama(trama))
+            throw new MaterialInvalido(nombre.toString() + " no admite " + trama.toString());
+
+        this.nombre = nombre;
+        this.trama = Objects.isNull(trama) ? TramaTela.LISA : trama;
+    }
+
+    /**
+     * RuntimeException Al crear un Material.
+     *
+     * @author Tomás Sánchez
+     * @version 1.0
+     * @since 2.0
+     */
+    public class MaterialInvalido extends RuntimeException {
+
+        /**
+         * Excepción al crear Material.
+         *
+         * @param causa la causa de invalidez
+         * @throws RuntimeException
+         */
+        public MaterialInvalido(String causa) {
+            super("Material inválido: " + causa);
+        }
+    }
+
 }
