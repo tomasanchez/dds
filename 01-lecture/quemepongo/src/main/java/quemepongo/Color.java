71852795cf065c0feae2149eac9d3e3157abfb6a
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-25 18:31:37 -0300

    refactor(qmp): Color iteration 2

---
+++ 01-lecture/quemepongo/src/main/java/quemepongo/Color.java
@@ -0,0 +1,18 @@
+package quemepongo;
+
+/**
+ * Dominio de colores
+ *
+ * @author Tomás Sánchez
+ * @since 04.25.2021
+ * @version 1.0
+ */
+public class Color {
+
+    /**
+     * El código hexadecimal.
+     *
+     * @since 1.0
+     */
+    String codigo;
+}
