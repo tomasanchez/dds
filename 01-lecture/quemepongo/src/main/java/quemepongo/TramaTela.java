7476f568dec9c937351a0f832e885c9d66d3fc9c
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-25 18:32:19 -0300

    feat(qmp): Trama for iteration 2

---
+++ 01-lecture/quemepongo/src/main/java/quemepongo/TramaTela.java
@@ -0,0 +1,11 @@
+package quemepongo;
+
+/**
+ * Dominio de Tramas del Material.
+ *
+ * @version 1.0
+ * @since 04.25.2021
+ */
+public enum TramaTela {
+    LISA, RAYADA, LUNARES, A_CUADROS, ESTAMPADO
+}
