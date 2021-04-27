09f7da50799e264842f6c167f277ea3e9e42c417
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-27 16:04:05 -0300

    test(qmp): actualizo la runtimeexception

--- 01-lecture/quemepongo/src/test/java/quemepongo/PrendaTest.java
+++ 01-lecture/quemepongo/src/test/java/quemepongo/PrendaTest.java
@@ -2,19 +2,21 @@ package quemepongo;
 
 import org.junit.jupiter.api.Test;
 
+import quemepongo.Prenda.PrendaInvalida;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PrendaTest {
 
     @Test
-    public void prendasValidas() {
+    public void noPrendasNulls() {
 
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(PrendaInvalida.class, () -> {
             new Prenda(null, null, null);
         });
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Prenda(null, null, null, null);
+        assertThrows(PrendaInvalida.class, () -> {
+            new Prenda(null, null, null, null, null);
         });
     }
 }
