d26af8b6bdebb4bad5fdd44d034ffb027a49261a
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-28 18:28:48 -0300

    test(qmp): prendas passed test

--- 01-lecture/quemepongo/src/test/java/quemepongo/PrendaTest.java
+++ 01-lecture/quemepongo/src/test/java/quemepongo/PrendaTest.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import quemepongo.Prenda.PrendaInvalida;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Testeo de Prendas
+ *
+ * @version 1.0
+ * @since 04.28.2021
+ */
 public class PrendaTest {
 
     @Test
-    public void noPrendasNulls() {
+    public void noPrendasVacias() {
 
         assertThrows(PrendaInvalida.class, () -> {
             new Prenda(null, null, null);
@@ -19,4 +26,21 @@ public class PrendaTest {
             new Prenda(null, null, null, null, null);
         });
     }
+
+    @Test
+    public void prendaConTramaDefaultLisa() {
+        assertTrue(prendaSinOnda(TipoPrenda.CAMISA, TipoMaterial.ALGODON).getTrama().equals(TramaTela.LISA));
+    }
+
+    /**
+     * Crea una prenda sin onda, lisa y gris.
+     *
+     * @param tipo     el tipo de Prenda
+     * @param material el tipo de material
+     * @return Una prenda sin onda: lisa y gris.
+     */
+    private static Prenda prendaSinOnda(TipoPrenda tipo, TipoMaterial material) {
+        return new Prenda(tipo, material, new Color("#gris"));
+    }
+
 }
