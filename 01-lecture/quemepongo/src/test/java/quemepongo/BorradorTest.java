f0c02bad9fa32d555c5462be0cedfdf631e2f550
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-28 18:52:06 -0300

    test(qmp): Borrador passes test

---
+++ 01-lecture/quemepongo/src/test/java/quemepongo/BorradorTest.java
@@ -0,0 +1,61 @@
+package quemepongo;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import quemepongo.Prenda.PrendaInvalida;
+
+public class BorradorTest {
+
+    @Test
+    public void noGuardarPrendasInvalidas() {
+        // No se guarda una prenda Vacía
+        assertThrows(PrendaInvalida.class, () -> {
+            new Borrador().guardarPrenda();
+        });
+    }
+
+    @Test
+    public void creaRopaBien() {
+
+        // Instancio y relleno una Prenda
+        Borrador bosquejo = new Borrador();
+        bosquejo.especificarTipo(TipoPrenda.CAMISA);
+        bosquejo.especificarMaterial(TipoMaterial.ALGODON);
+        bosquejo.especificarColorPrimario(colorPrueba());
+
+        Prenda camisaAlgodonGris = new Prenda(TipoPrenda.CAMISA, TipoMaterial.ALGODON, colorPrueba());
+        Prenda guardada = bosquejo.guardarPrenda();
+
+        // Mismo color
+        assertTrue(camisaAlgodonGris.color1.codigo.equals(guardada.color1.codigo));
+        // Misma trama y además la trama es LISA.
+        assertTrue(camisaAlgodonGris.getTrama().equals(guardada.getTrama()) && esTramaDefault(guardada));
+        // Mismo material
+        assertTrue(camisaAlgodonGris.material.nombre.equals(guardada.material.nombre));
+        // Mismo tipo
+        assertTrue(camisaAlgodonGris.tipo.equals(guardada.tipo));
+    }
+
+    /**
+     * Instancia un color de prueba.
+     *
+     * @return un color válido.
+     */
+    private Color colorPrueba() {
+        return new Color("#gris");
+    }
+
+    /**
+     * Verifica si posee la trama default.
+     *
+     * @param prenda la prenda a verificar
+     * @return si posee la trama LISA.
+     */
+    private boolean esTramaDefault(Prenda prenda) {
+        return prenda.getTrama().equals(TramaTela.LISA);
+    }
+
+}
