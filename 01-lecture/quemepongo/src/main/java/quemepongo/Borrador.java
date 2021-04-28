7b298599da2b93e4d9a6d1a4776d8710a5d710b8
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-28 16:38:31 -0300

    feat(qmp): Prenda builder

--- 01-lecture/quemepongo/src/main/java/quemepongo/Borrador.java
+++ 01-lecture/quemepongo/src/main/java/quemepongo/Borrador.java
@@ -45,27 +45,57 @@ public class Borrador {
         }
     }
 
+    /**
+     * Setter de tipo de prenda.
+     *
+     * @param tipo el tipo de la prenda.
+     */
     public void especificarTipo(TipoPrenda tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Setter de material
+     *
+     * @param material el Tipo de material
+     */
     public void especificarMaterial(TipoMaterial material) {
         this.material = material;
     }
 
+    /**
+     * Setter de color primario.
+     *
+     * @param color1 el color principal de la prenda.
+     */
     public void especificarColorPrimario(Color color1) {
         this.color1 = color1;
     }
 
+    /**
+     * Setter de color secundario.
+     *
+     * @param color2 el color secundario.
+     */
     public void especificarColorSecundario(Color color2) {
         this.color2 = color2;
     }
 
+    /**
+     * Setter de trama.
+     *
+     * @param trama la trama del material de una prenda.
+     */
     public void especificarTrama(TramaTela trama) {
         this.trama = trama;
     }
 
-    public Prenda crearPrenda() {
+    /**
+     * De ser posible guarda la prenda borrador.
+     *
+     * @since 1.0
+     */
+    public Prenda guardarPrenda() {
         return new Prenda(tipo, material, trama, color1, color2);
     }
 }
