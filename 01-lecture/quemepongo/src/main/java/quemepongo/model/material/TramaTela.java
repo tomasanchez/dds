c2a0a5c2f2b234a6f51dfbe10e6a5856a6c26cca
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-17 13:23:35 -0300

    docs(checkstyle): enum details

--- 01-lecture/quemepongo/src/main/java/quemepongo/model/material/TramaTela.java
+++ 01-lecture/quemepongo/src/main/java/quemepongo/model/material/TramaTela.java
@@ -3,9 +3,27 @@ package quemepongo.model.material;
 /**
  * Dominio de Tramas del Material.
  *
- * @version 1.0
  * @since 04.25.2021
  */
 public enum TramaTela {
-    LISA, RAYADA, LUNARES, A_CUADROS, ESTAMPADO
+    /**
+     * Tipo de trama Liso.
+     */
+    LISA,
+    /**
+     * Tipo de trama Rayado.
+     */
+    RAYADA,
+    /**
+     * Tipo de trama A Lunares.
+     */
+    LUNARES,
+    /**
+     * Tipo de trama A Cuadros.
+     */
+    A_CUADROS,
+    /**
+     * Tipo de trama Estampado.
+     */
+    ESTAMPADO
 }
