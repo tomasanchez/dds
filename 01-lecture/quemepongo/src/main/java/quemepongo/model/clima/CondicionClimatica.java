e57d0ebcfcf93e941ded2976c08655e69342685b
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-20 14:58:44 -0300

    refactor(condicion): clima setter

--- 01-lecture/quemepongo/src/main/java/quemepongo/model/clima/CondicionClimatica.java
+++ 01-lecture/quemepongo/src/main/java/quemepongo/model/clima/CondicionClimatica.java
@@ -1,17 +1,29 @@
 package quemepongo.model.clima;
 
+/**
+ * Reporte de temperatura, probabilidad de lluvia y clima.
+ *
+ * @since 05.18.2021
+ * @version 1.0
+ */
 public class CondicionClimatica {
 
     private Temperatura temperatura = new Temperatura();
-    private Clima clima;
+    private Clima clima = Clima.FRIO;
     private double precipitaciones;
 
     public CondicionClimatica() {}
 
+    /**
+     * Instancia una Condicion.
+     *
+     * @param temperatura la temperatura registrada
+     * @param precipitaciones la probabilidad de precipitaciones.
+     */
     public CondicionClimatica(Temperatura temperatura, double precipitaciones) {
         this.temperatura = temperatura;
         this.precipitaciones = precipitaciones;
-        this.clima = temperatura.determinarClima();
+        this.clima = Clima.determinarClima(temperatura);
     }
 
     public Temperatura getTemperatura() {
