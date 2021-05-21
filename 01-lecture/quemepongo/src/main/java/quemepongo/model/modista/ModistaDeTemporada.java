84e2b0edcbcd60b95c08454340a02f4a31727368
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-21 10:22:48 -0300

    feat(modista): new modista

---
+++ 01-lecture/quemepongo/src/main/java/quemepongo/model/modista/ModistaDeTemporada.java
@@ -0,0 +1,37 @@
+package quemepongo.model.modista;
+
+import quemepongo.model.prenda.Prenda;
+
+/**
+ * Factory de Atuendos de acuerdo al clima actual.
+ *
+ * @version 2.0
+ * @since 04.27.2021
+ */
+public class ModistaDeTemporada extends Modista {
+
+    @Override
+    protected Prenda fabricarPrendaSuperior() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Prenda fabricarPrendaInferior() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Prenda fabricarCalzado() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Prenda fabricarAccesorio() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
