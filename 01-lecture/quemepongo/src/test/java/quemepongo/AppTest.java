cf6dddf4d2a7f0afbcbda996464d71b6fb6eb300
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-21 10:22:31 -0300

    refactor(recomendador): import path

--- 01-lecture/quemepongo/src/test/java/quemepongo/AppTest.java
+++ 01-lecture/quemepongo/src/test/java/quemepongo/AppTest.java
@@ -1,7 +1,7 @@
 package quemepongo;
 
 import org.junit.jupiter.api.Test;
-import quemepongo.model.modista.Recomendador;
+import quemepongo.service.prenda.Recomendador;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
