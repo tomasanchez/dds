518bc3fee361f27fae45f83a3e74d0a58057f170
Tomas <tosanchez@frba.utn.edu.ar>
2021-06-08 17:23:22 -0300

    feat(it-6): add domain alerts

---
+++ 01-lecture/quemepongo/src/main/java/quemepongo/model/clima/AlertaClimatica.java
@@ -0,0 +1,16 @@
+package quemepongo.model.clima;
+
+public enum AlertaClimatica {
+    /**
+     * Alerta de tormenta.
+     */
+    TORMENTA,
+    /**
+     * Alerta de tormenta.
+     */
+    GRANIZO,
+    /**
+     * Alerta de nevadas.
+     */
+    NEVADA;
+}
